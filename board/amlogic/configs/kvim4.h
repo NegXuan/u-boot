2f0ca72b5139b8f44fd2378ce2b82329747ca9e6
Mark Wan <mark.wan@namtso.com>
2024-10-16 14:51:13 +0800

    VIM4/4N: add support for logo
    Signed-off-by: Mark Wan <mark.wan@namtso.com>
    Change-Id: I3f3f747fbec2390aed0659b3f890d99700f33f3c

--- board/amlogic/configs/kvim4.h
+++ board/amlogic/configs/kvim4.h
@@ -126,8 +126,8 @@
         "display_width=1920\0" \
         "display_height=1080\0" \
 		"hdmichecksum=0x00000000\0" \
-		"display_bpp=16\0" \
-        "display_color_index=16\0" \
+        "display_bpp=24\0" \
+        "display_color_index=24\0" \
         "display_layer=osd0\0" \
         "display_color_fg=0xffff\0" \
         "display_color_bg=0\0" \
