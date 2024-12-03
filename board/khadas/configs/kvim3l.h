92088ffd1e0a2d09c71b75f654f99803c9cc38e5
Reeve Huang <reeve.huang@wesion.com>
2024-12-03 10:30:29 +0800

    VIM3L: enable command line interface editing and auto-completion
    Signed-off-by: Reeve Huang <reeve.huang@wesion.com>
    Change-Id: I9f70460b4ca4fd5733a067a2e2b4ac2ac63345cf

--- board/khadas/configs/kvim3l.h
+++ board/khadas/configs/kvim3l.h
@@ -780,6 +780,11 @@
 #define CONFIG_CPU_CLK_DEBUG        600
 #endif
 
+//Command line interface
+#define CONFIG_CMDLINE            1
+#define CONFIG_CMDLINE_EDITING    1
+#define CONFIG_AUTO_COMPLETE      1
+
 //support secure boot
 #define CONFIG_AML_SECURE_UBOOT   1
 
